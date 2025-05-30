29784bcc4f0392f5814c1b5c4efa0c21d5cd3351
hsedo777 <hsedo777@gmail.com>
2025-05-30 18:03:41 +0100

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,58 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "org.sedo.satmesh"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "org.sedo.satmesh"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    buildFeatures{
+        viewBinding = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+
+    // Room
+    implementation(libs.androidx.room.runtime)
+    annotationProcessor(libs.room.compiler)
+    testImplementation(libs.androidx.room.testing)
+    //SQLCipher for database file.s encryption/decryption
+    implementation(libs.sqlcipher.android)
+    //Protocol Buffers
+    implementation(libs.protobuf.javalite)
+    // Signal Protocol
+    implementation(libs.libsignal.protocol.java)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}

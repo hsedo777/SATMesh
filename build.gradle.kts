7a2e3dfa49c5cb6f9bcab59e58b688d44e59f768
hsedo777 <hsedo777@gmail.com>
2025-06-26 22:42:28 +0100

    Update library room's version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
-    id("androidx.room") version "2.7.1" apply false
+    id("androidx.room") version "2.7.2" apply false
     id("com.google.protobuf") version "0.9.5" apply false
 }

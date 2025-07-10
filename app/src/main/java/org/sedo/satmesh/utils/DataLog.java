72f40185df12c480005d41d18cc8b7cd0eecdb8a
hsedo777 <hsedo777@gmail.com>
2025-07-10 15:16:29 +0100

    Add logging class 'DataLog'

---
+++ app/src/main/java/org/sedo/satmesh/utils/DataLog.java
@@ -0,0 +1,13 @@
+package org.sedo.satmesh.utils;
+
+/**
+ * Manages the logging of performance and behavior data for the SATMesh application
+ * into a dedicated file. This allows for subsequent automated analysis.
+ * It uses a structured format for each log entry to facilitate parsing.
+ * <p>
+ * This class ensures thread-safe operations for logging.
+ *
+ * @author hovozounkou
+ */
+public class DataLog {
+}

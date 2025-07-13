aef3731a06a10693dbaad0e36b0565cdd94b4a72
hsedo777 <hsedo777@gmail.com>
2025-07-13 04:44:54 +0100

    Downgrade version of protocol buffers to match protocol Signal required version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,7 @@ android {
 
 protobuf {
     protoc {
-        artifact = "com.google.protobuf:protoc:3.25.3"
+        artifact = "com.google.protobuf:protoc:3.10.0"
     }
     generateProtoTasks {
         all().forEach { task ->

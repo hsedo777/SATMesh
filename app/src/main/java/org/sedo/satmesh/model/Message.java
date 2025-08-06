645d22654bc6906fa91d74b736350728be35ba9b
hsedo777 <hsedo777@gmail.com>
2025-08-06 21:45:11 +0100

    Fix control on the new payload ID to set on a message

--- app/src/main/java/org/sedo/satmesh/model/Message.java
+++ app/src/main/java/org/sedo/satmesh/model/Message.java
@@ -168,7 +168,7 @@ public class Message {
 	}
 
 	public void setPayloadId(Long payloadId) {
-		if ((payloadId != null && payloadId == 0L)
+		if ((payloadId == null || payloadId == 0L)
 				|| (this.payloadId != null && this.payloadId != 0L) //Prevent payload ID modification
 		) {
 			return;

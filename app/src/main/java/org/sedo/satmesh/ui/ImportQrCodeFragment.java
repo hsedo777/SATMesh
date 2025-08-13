998946173edb8a687275e7b57fe852eefb0ee843
hsedo777 <hsedo777@gmail.com>
2025-08-14 00:28:09 +0100

    Handle all predefined cases of QR code import

--- app/src/main/java/org/sedo/satmesh/ui/ImportQrCodeFragment.java
+++ app/src/main/java/org/sedo/satmesh/ui/ImportQrCodeFragment.java
@@ -25,9 +25,12 @@ import com.journeyapps.barcodescanner.ScanOptions;
 
 import org.sedo.satmesh.R;
 import org.sedo.satmesh.databinding.FragmentImportQrCodeBinding;
+import org.sedo.satmesh.proto.QrMessage;
 import org.sedo.satmesh.ui.vm.ImportQrCodeViewModel;
 import org.sedo.satmesh.ui.vm.ViewModelFactory;
 import org.sedo.satmesh.utils.Constants;
+
+import java.util.Objects;
 
 public class ImportQrCodeFragment extends Fragment {
 
@@ -58,7 +61,11 @@ public class ImportQrCodeFragment extends Fragment {
 					Snackbar.make(binding.getRoot(), R.string.no_qr_code_detected, Toast.LENGTH_SHORT).show();
 				}
 			});
+
+	// Listeners
 	private AppHomeListener homeListener;
+	private QrCodeGenerationListener qrCodeGenerationListener;
+	private DiscussionListener discussionListener;
 
 	public ImportQrCodeFragment() {
 		// Required default constructor
@@ -82,10 +89,14 @@ public class ImportQrCodeFragment extends Fragment {
 	@Override
 	public void onAttach(@NonNull Context context) {
 		super.onAttach(context);
-		if (context instanceof AppHomeListener) {
+		if (context instanceof AppHomeListener && context instanceof QrCodeGenerationListener &&
+				context instanceof DiscussionListener) {
 			homeListener = (AppHomeListener) context;
+			qrCodeGenerationListener = (QrCodeGenerationListener) context;
+			discussionListener = (DiscussionListener) context;
 		} else {
-			throw new RuntimeException("The context must implement AppHomeListener");
+			throw new RuntimeException("The context must implement `AppHomeListener`," +
+					" `QrCodeGenerationListener` and `DiscussionListener`");
 		}
 	}
 
@@ -93,6 +104,8 @@ public class ImportQrCodeFragment extends Fragment {
 	public void onDetach() {
 		super.onDetach();
 		homeListener = null;
+		qrCodeGenerationListener = null;
+		discussionListener = null;
 	}
 
 	@Nullable
@@ -111,39 +124,23 @@ public class ImportQrCodeFragment extends Fragment {
 		}
 
 		// Observers
-		viewModel.getQrSource().observe(getViewLifecycleOwner(), source -> {
-			if (source != null) {
-				binding.textQrCodeSource.setText(source);
-				binding.qrCodeSourceContainer.setVisibility(View.VISIBLE);
-			} else {
-				binding.qrCodeSourceContainer.setVisibility(View.GONE);
-			}
-		});
+		setupObservers();
 
-		viewModel.getPreviewBitmap().observe(getViewLifecycleOwner(), bitmap -> {
-			if (bitmap != null) {
-				binding.imageQrPreview.setImageBitmap(bitmap);
-				binding.imageQrPreview.setVisibility(View.VISIBLE);
-			} else {
-				binding.imageQrPreview.setVisibility(View.GONE);
-			}
-		});
-
-		viewModel.getErrorMessage().observe(getViewLifecycleOwner(), message -> {
-			if (message != null) {
-				Snackbar.make(binding.getRoot(), message, Snackbar.LENGTH_SHORT).show();
-				binding.textQrError.setText(message);
-				binding.textQrError.setVisibility(View.VISIBLE);
-			} else {
-				binding.textQrError.setVisibility(View.GONE);
-			}
-		});
+		// Click events
+		setupClickEvent();
 
 		// Toolbar actions
 		binding.toolbarImportQrCode.setOnMenuItemClickListener(this::handleMenuItem);
+		binding.toolbarImportQrCode.setNavigationOnClickListener(v -> {
+			Log.d(TAG, "NavigationOnClickListener: homeListener=" + homeListener);
+			if (homeListener != null) {
+				homeListener.backToHome();
+			}
+		});
 		requireActivity().getOnBackPressedDispatcher().addCallback(new OnBackPressedCallback(true) {
 			@Override
 			public void handleOnBackPressed() {
+				Log.d(TAG, "handleOnBackPressed: homeListener=" + homeListener);
 				if (homeListener != null) {
 					homeListener.backToHome();
 				}
@@ -178,6 +175,76 @@ public class ImportQrCodeFragment extends Fragment {
 	// Import from gallery
 	private void pickImageFromGallery() {
 		pickImageLauncher.launch("image/*");
+	}
+
+	private void setupObservers() {
+		viewModel.getPreviewBitmap().observe(getViewLifecycleOwner(), bitmap -> {
+			if (bitmap != null) {
+				binding.imageQrPreview.setImageBitmap(bitmap);
+				binding.imageQrPreview.setVisibility(View.VISIBLE);
+			} else {
+				binding.imageQrPreview.setVisibility(View.GONE);
+			}
+		});
+
+		viewModel.getErrorMessage().observe(getViewLifecycleOwner(), message -> {
+			if (message != null) {
+				Snackbar.make(binding.getRoot(), message, Snackbar.LENGTH_SHORT).show();
+				binding.textQrError.setText(message);
+				binding.textQrError.setVisibility(View.VISIBLE);
+				binding.qrCodeActionButton.setEnabled(false);
+			} else {
+				binding.textQrError.setVisibility(View.GONE);
+			}
+		});
+
+		viewModel.getQrMessageSource().observe(getViewLifecycleOwner(), qrMessage -> {
+			binding.qrCodeActionButton.setEnabled(qrMessage != null);
+			if (qrMessage != null) {
+				binding.textQrCodeSource.setText(qrMessage.getSourceUuid());
+				binding.qrCodeSourceContainer.setVisibility(View.VISIBLE);
+			} else {
+				binding.qrCodeSourceContainer.setVisibility(View.GONE);
+			}
+		});
+
+		viewModel.getIsTaskOnExecution().observe(getViewLifecycleOwner(),
+				isOnExecution -> binding.importQrProgressBar.setVisibility(isOnExecution ? View.VISIBLE : View.GONE));
+	}
+
+	private void setupClickEvent() {
+		binding.textQrCodeSource.setOnClickListener(v -> {
+			QrMessage qrMessage = viewModel.getQrMessageSource().getValue();
+			String uuid = qrMessage == null ? null : qrMessage.getSourceUuid();
+			if (uuid != null) {
+				if (UiUtils.copyToClipboard(requireContext(), uuid, getString(R.string.qr_code_source_label))) {
+					Snackbar.make(binding.getRoot(), R.string.qr_code_source_copied, Snackbar.LENGTH_SHORT).show();
+				}
+			}
+		});
+
+		binding.qrCodeActionButton.setOnClickListener(v -> viewModel.processQrMessage(success -> {
+			if (success) {
+				// On success, the `QrMessage` exists
+				QrMessage qrMessage = Objects.requireNonNull(viewModel.getQrMessageSource().getValue());
+				if (qrMessage.getType() == QrMessage.MessageType.PRE_KEY_BUNDLE.getNumber()) {
+					if (qrCodeGenerationListener != null) {
+						qrCodeGenerationListener.generatedAcknowledgeTo(qrMessage.getSourceUuid());
+					}
+				} else if (qrMessage.getType() == QrMessage.MessageType.PERSONAL_INFO.getNumber()) {
+					viewModel.retrieveNodeAsync(qrMessage.getSourceUuid(), node -> {
+						if (discussionListener != null && node != null) {
+							discussionListener.discussWith(node, true);
+						} else {
+							Log.w(TAG, "Failed to init discussion with the node: " + node);
+						}
+					});
+				}
+			} else if (viewModel.getErrorMessage().getValue() == null) {
+				Snackbar.make(binding.getRoot(), R.string.qr_code_process_failed, Snackbar.LENGTH_SHORT).show();
+			}
+			// Else, an error message is displayed.
+		}));
 	}
 
 	@Override
--- app/src/main/java/org/sedo/satmesh/ui/vm/ImportQrCodeViewModel.java
+++ app/src/main/java/org/sedo/satmesh/ui/vm/ImportQrCodeViewModel.java
@@ -1,10 +1,15 @@
 package org.sedo.satmesh.ui.vm;
 
+import static org.sedo.satmesh.signal.SignalManager.getAddress;
+import static org.sedo.satmesh.signal.SignalManager.toCiphertextMessage;
+
 import android.app.Application;
 import android.content.ContentResolver;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
@@ -21,60 +26,94 @@ import com.google.zxing.Result;
 import com.google.zxing.common.HybridBinarizer;
 
 import org.sedo.satmesh.R;
+import org.sedo.satmesh.model.Node;
+import org.sedo.satmesh.proto.PersonalInfo;
 import org.sedo.satmesh.proto.QrIdentity;
+import org.sedo.satmesh.proto.QrMessage;
+import org.sedo.satmesh.signal.SignalManager;
+import org.sedo.satmesh.ui.data.NodeRepository;
+import org.sedo.satmesh.ui.data.NodeRepository.NodeCallback;
+import org.whispersystems.libsignal.InvalidKeyException;
+import org.whispersystems.libsignal.UntrustedIdentityException;
 
 import java.io.InputStream;
 import java.util.Objects;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.function.Consumer;
 
 public class ImportQrCodeViewModel extends AndroidViewModel {
 
 	private static final String TAG = "ImportQrCodeVM";
 
 	private final MutableLiveData<Bitmap> previewBitmap = new MutableLiveData<>();
-	private final MutableLiveData<String> qrSource = new MutableLiveData<>();
 	private final MutableLiveData<String> errorMessage = new MutableLiveData<>();
+	private final MutableLiveData<QrMessage> qrMessageSource = new MutableLiveData<>();
+	private final MutableLiveData<Boolean> isTaskOnExecution = new MutableLiveData<>(false);
+
+	private final ExecutorService executor = Executors.newSingleThreadExecutor();
+	private final Handler handler = new Handler(Looper.getMainLooper());
+	private final SignalManager signalManager;
+	private final NodeRepository nodeRepository;
 	private String hostNodeAddressName;
 
 	protected ImportQrCodeViewModel(Application application) {
 		super(application);
+		signalManager = SignalManager.getInstance(application);
+		nodeRepository = new NodeRepository(application);
 	}
 
 	public LiveData<Bitmap> getPreviewBitmap() {
 		return previewBitmap;
 	}
 
-	public LiveData<String> getQrSource() {
-		return qrSource;
-	}
-
 	public LiveData<String> getErrorMessage() {
 		return errorMessage;
+	}
+
+	public LiveData<QrMessage> getQrMessageSource() {
+		return qrMessageSource;
 	}
 
 	public void setHostNodeAddressName(String hostNodeAddressName) {
 		this.hostNodeAddressName = hostNodeAddressName;
 	}
 
+	public MutableLiveData<Boolean> getIsTaskOnExecution() {
+		return isTaskOnExecution;
+	}
+
 	private void parseQrCode(String base64QrCode) {
 		try {
 			byte[] qrCodeBytes = android.util.Base64.decode(base64QrCode, android.util.Base64.NO_WRAP);
-			QrIdentity identity = QrIdentity.parseFrom(qrCodeBytes);
-			qrSource.setValue(identity.getSourceUuid());
-			if (Objects.equals(identity.getDestinationUuid(), identity.getSourceUuid())) {
-				Log.w(TAG, "QR code self targeting, ID=" + identity.getSourceUuid());
-				errorMessage.setValue(getApplication().getString(R.string.qr_code_self_targeting));
-				return;
+			QrMessage qrMessage = QrMessage.parseFrom(qrCodeBytes);
+			if (qrMessage.getType() == QrMessage.MessageType.PRE_KEY_BUNDLE.getNumber()) {
+				if (Objects.equals(qrMessage.getDestinationUuid(), qrMessage.getSourceUuid())) {
+					Log.w(TAG, "QR code self targeting, ID=" + qrMessage.getSourceUuid());
+					errorMessage.postValue(getApplication().getString(R.string.qr_code_self_targeting));
+					return;
+				}
+				if (!Objects.equals(qrMessage.getDestinationUuid(), hostNodeAddressName)) {
+					Log.w(TAG, "PRE_KEY_BUNDLE: QR code stealing, it's originally designated to '" + qrMessage.getDestinationUuid() +
+							"' but '" + hostNodeAddressName + "' is decoding it.");
+					errorMessage.postValue(getApplication().getString(R.string.qr_code_stealing_use));
+					return;
+				}
+				qrMessageSource.postValue(qrMessage);
+			} else if (qrMessage.getType() == QrMessage.MessageType.PERSONAL_INFO.getNumber()) {
+				if (!Objects.equals(hostNodeAddressName, qrMessage.getDestinationUuid())) {
+					Log.w(TAG, "PERSONAL_INFO: QR code stealing, it's originally designated to '" + qrMessage.getDestinationUuid() +
+							"' but '" + hostNodeAddressName + "' is decoding it.");
+					errorMessage.postValue(getApplication().getString(R.string.qr_code_stealing_use));
+					return;
+				}
+				qrMessageSource.postValue(qrMessage);
 			}
-			if (!Objects.equals(identity.getDestinationUuid(), hostNodeAddressName)) {
-				Log.w(TAG, "QR code stealing, it's originally designated to '" + identity.getDestinationUuid() +
-						"' but '" + hostNodeAddressName + "' is decoding it.");
-				errorMessage.setValue(getApplication().getString(R.string.qr_code_stealing_use));
-				return;
-			}
-			errorMessage.setValue(null);
 		} catch (InvalidProtocolBufferException e) {
 			Log.e(TAG, "Failed to parse QR code", e);
-			errorMessage.setValue(getApplication().getString(R.string.qr_code_invalid_content));
+			errorMessage.postValue(getApplication().getString(R.string.qr_code_invalid_content));
+		} finally {
+			isTaskOnExecution.postValue(false);
 		}
 	}
 
@@ -91,11 +130,11 @@ public class ImportQrCodeViewModel extends AndroidViewModel {
 	 */
 	public void decodeFromBitmap(Bitmap bitmap) {
 		if (bitmap == null) {
-			errorMessage.setValue(getApplication().getString(R.string.no_image_detected));
+			errorMessage.postValue(getApplication().getString(R.string.no_image_detected));
 			return;
 		}
 
-		previewBitmap.setValue(bitmap);
+		previewBitmap.postValue(bitmap);
 		try {
 			int width = bitmap.getWidth();
 			int height = bitmap.getHeight();
@@ -109,8 +148,7 @@ public class ImportQrCodeViewModel extends AndroidViewModel {
 			parseQrCode(result.getText());
 		} catch (Exception e) {
 			Log.e(TAG, "Decoding failed", e);
-			qrSource.setValue(null);
-			errorMessage.setValue(getApplication().getString(R.string.qr_code_decode_failed));
+			errorMessage.postValue(getApplication().getString(R.string.qr_code_decode_failed));
 		}
 	}
 
@@ -127,7 +165,7 @@ public class ImportQrCodeViewModel extends AndroidViewModel {
 			}
 		} catch (Exception e) {
 			Log.e(TAG, "Failed to load image", e);
-			errorMessage.setValue(getApplication().getString(R.string.qr_code_import_from_image_failed));
+			errorMessage.postValue(getApplication().getString(R.string.qr_code_import_from_image_failed));
 		}
 	}
 
@@ -135,8 +173,124 @@ public class ImportQrCodeViewModel extends AndroidViewModel {
 	 * Reset the current state.
 	 */
 	public void reset() {
-		previewBitmap.setValue(null);
-		qrSource.setValue(null);
-		errorMessage.setValue(null);
+		previewBitmap.postValue(null);
+		errorMessage.postValue(null);
+		qrMessageSource.postValue(null);
+	}
+
+	public void processQrMessage(@NonNull Consumer<Boolean> callback) {
+		try {
+			QrMessage qrMessage = qrMessageSource.getValue();
+			if (qrMessage == null) {
+				Log.e(TAG, "QR message is null");
+				callback.accept(false);
+				return;
+			}
+			if (qrMessage.getType() == QrMessage.MessageType.PRE_KEY_BUNDLE.getNumber()) {
+				executor.execute(() -> {
+					handler.post(() -> isTaskOnExecution.postValue(true));
+					try {
+						QrIdentity identity = QrIdentity.parseFrom(qrMessage.getData());
+						signalManager.establishSessionFromRemotePreKeyBundle(
+								getAddress(qrMessage.getSourceUuid()),
+								signalManager.deserializePreKeyBundle(identity.getPreKeyBundle().toByteArray()));
+						Log.d(TAG, "Signal secure session established successfully by '" +
+								hostNodeAddressName + "' with '" + qrMessage.getSourceUuid() + "'.");
+						handler.post(() -> callback.accept(true));
+					} catch (InvalidProtocolBufferException e) {
+						Log.e(TAG, "The identity data may have been malformed.", e);
+						handler.post(() -> {
+							errorMessage.postValue(getApplication().getString(R.string.qr_code_invalid_content));
+							callback.accept(false);
+						});
+					} catch (InvalidKeyException | UntrustedIdentityException e) {
+						Log.e(TAG, "Failed to establish the Signal secure session.", e);
+						handler.post(() -> {
+							errorMessage.postValue(getApplication().getString(R.string.qr_code_secure_session_failed));
+							callback.accept(false);
+						});
+					} catch (Exception e) {
+						Log.e(TAG, "Failed to establish the Signal secure session.", e);
+						handler.post(() -> {
+							errorMessage.postValue(getApplication().getString(R.string.qr_code_process_failed));
+							callback.accept(false);
+						});
+					} finally {
+						handler.post(() -> isTaskOnExecution.postValue(false));
+					}
+				});
+			} else if (qrMessage.getType() == QrMessage.MessageType.PERSONAL_INFO.getNumber()) {
+				executor.execute(() -> {
+					handler.post(() -> isTaskOnExecution.postValue(true));
+					try {
+						byte[] decryptedData = signalManager.decryptMessage(
+								getAddress(qrMessage.getSourceUuid()),
+								toCiphertextMessage(qrMessage.getData().toByteArray()));
+						PersonalInfo info = PersonalInfo.parseFrom(decryptedData);
+						Log.d(TAG, "Personal information decrypted successfully.");
+						nodeRepository.findOrCreateNodeAsync(info.getAddressName(), new NodeCallback() {
+							@Override
+							public void onNodeReady(@NonNull Node node) {
+								Log.d(TAG, "Node found or created successfully.");
+								node.setPersonalInfo(info);
+								nodeRepository.update(node, success -> {
+									if (success) {
+										handler.post(() -> {
+											isTaskOnExecution.postValue(false);
+											callback.accept(true);
+										});
+									} else {
+										handler.post(() -> {
+											ImportQrCodeViewModel.this.errorMessage.postValue(getApplication().getString(R.string.qr_code_process_failed));
+											isTaskOnExecution.postValue(false);
+											callback.accept(false);
+										});
+									}
+								});
+							}
+
+							@Override
+							public void onError(@NonNull String errorMessage) {
+								Log.e(TAG, "Failed to decrypt the personal information: " + errorMessage);
+								handler.post(() -> {
+									ImportQrCodeViewModel.this.errorMessage.postValue(getApplication().getString(R.string.qr_code_process_failed));
+									isTaskOnExecution.postValue(false);
+									callback.accept(true);
+								});
+							}
+						});
+					} catch (Exception e) {
+						Log.e(TAG, "Failed to decrypt the personal information.", e);
+						handler.post(() -> {
+							errorMessage.postValue(getApplication().getString(R.string.qr_code_invalid_content));
+							isTaskOnExecution.postValue(false);
+							callback.accept(false);
+						});
+					}
+				});
+			} else {
+				Log.e(TAG, "Unknown QR message type: " + qrMessage.getType());
+				callback.accept(false);
+			}
+		} catch (Exception e) {
+			errorMessage.postValue(getApplication().getString(R.string.qr_code_secure_session_failed));
+			callback.accept(false);
+			Log.w(TAG, "Failed to established the Signal secure session.", e);
+		}
+	}
+
+	public void retrieveNodeAsync(String addressName, Consumer<Node> onReady) {
+		try {
+			Node node = nodeRepository.findNodeSync(addressName);
+			handler.post(() -> onReady.accept(node));
+		} catch (Exception e) {
+			handler.post(() -> onReady.accept(null));
+		}
+	}
+
+	@Override
+	protected void onCleared() {
+		super.onCleared();
+		executor.shutdown();
 	}
 }
